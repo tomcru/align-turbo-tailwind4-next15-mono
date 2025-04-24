2f54d401e5fcf078a1a7536a455964b987467c2b
tomcru <hello@tomrumpf.com>
2025-04-24 13:46:45 +0200

    chore: clean up

--- apps/web/app/store.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai';
-import type { JSX } from 'react';
-
-export const modalAtom = atom<JSX.Element | null>(null);
-export const toastAtom = atom<JSX.Element | null>(null);
